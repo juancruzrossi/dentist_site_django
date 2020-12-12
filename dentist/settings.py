043ae89b88a9f24f37aaba8ccc575f4429f1e70c
Juan Cruz <jcrzrossi@gmail.com>
2020-12-12 00:28:14 -0300

    minor changes

--- dentist/settings.py
+++ dentist/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Email Settings
